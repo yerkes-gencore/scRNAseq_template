edce58553b824b9bde10e7f7796938f4efbd2f38
Derrik Gratz <derrik.gratz@gmail.com>
2023-07-14 14:00:31 -0400

    init findMarkers background script

---
+++ R/findMarkers.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+## This script is intended to be ran as a background job using a processed
+## Seurat object with clusters set
+
+library(Seurat)
+library(here)
+
+MAST_installed <- require('MAST')
+if (MAST_installed){
+  DGE_method <- 'MAST'
+} else {
+  DGE_method <- 'wilcox'
+}
+
+here::i_am('R/findMarkers.R')
+
+# Load data ----
+seurat_obj <- readRDS(here::here('saved_rds/obj_post-reclustering.Rds'))
+
+# Finding markers ----
+## Run find markers with desired thresholds
+cluster_markers <- Seurat::FindAllMarkers(seurat_obj, assay = 'RNA', return.thresh = 0.01, min.pct = 0.25, densify = TRUE, test.use = DGE_method)
+
+# Save results ----
+saveRDS(cluster_markers, here::here('saved_rds/cluster_markers_test.Rds'), compress = FALSE)
