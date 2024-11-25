050a71c384ec8678d7491ea6c3a2306361ff5cd3
akcorut <kivanc.corut@gmail.com>
2024-11-25 14:33:34 -0500

    Update renv_setup.R to install additional necessary packages

--- helper_scripts/renv_setup.R
+++ helper_scripts/renv_setup.R
@@ -6,8 +6,8 @@
 ## Or if scrapping a corrupt renv setup, after running `renv::deactivate(clean=TRUE)`
 
 # Make sure R package manager repo is up-to-date
-options(repos = "https://packagemanager.rstudio.com/all/__linux__/focal/latest")
-
+# options(repos = "https://packagemanager.rstudio.com/all/__linux__/focal/latest")
+renv::install("remotes")
 ## If using R 4.3.1 [[ NOTE: You can set rebuild = FALSE (Default) since I've done this and it's in the global cache ]]
 remotes::install_version("Matrix", version = "1.6-4") # see https://github.com/plger/scDblFinder/issues/91#issuecomment-1825482069
 renv::install("irlba", rebuild = TRUE, prompt = FALSE)
@@ -16,6 +16,15 @@ renv::install("SeuratObject@5.0.1", rebuild = TRUE, prompt = FALSE) # also see h
 ## Error: package or namespace load failed for ‘celldex’ in loadNamespace(i, c(lib.loc, .libPaths()), versionCheck = vI[[i]]): namespace ‘DBI’ 1.1.3 is already loaded, but >= 1.2.0 is required
 renv::install("DBI@1.2.0")
 
+## Install the gencore packages.
+renv::install("yerkes-gencore/gencoreSC")
+renv::install("yerkes-gencore/gencoreBulk")
+
+## Install loupeR from github repo
+renv::install("10XGenomics/loupeR")
+
+## 
+renv::install(c("boot", "nnet", "writexl"))
 
 ## Initialize, and setup renv to work with bioconductor
 renv::init(bioconductor = TRUE)
